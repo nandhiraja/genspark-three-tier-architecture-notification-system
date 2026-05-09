75732ac994a99e33ec2722bcfeaeb823b844d409
nandhiraja <nandhiraja16@gmail.com>
2026-05-09 21:20:59 +0530

    feat(DAL): initial setup of data access layer and repositories

---
+++ NotificationSystemApplication.DAL/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace NotificationSystemApplication.DAL;
+
+public class Class1
+{
+
+}
---
+++ NotificationSystemApplication.DAL/Repositories/MessageRepository.cs
@@ -0,0 +1,66 @@
+using NotificationSystemApplication.Core.CustomExceptions;
+using NotificationSystemApplication.Core.Interfaces;
+using NotificationSystemApplication.Core.Models;
+
+namespace NotificationSystemApplication.DAL.Repositories
+{
+    // Repository for managing messages
+    public class MessageRepository : IMessageRepository
+    {
+        static Dictionary<string, List<Message>> _messageDataBase = new Dictionary<string, List<Message>>();
+
+        public void AddUserMessage(User user, Message message)
+        {   
+            if(!_messageDataBase.ContainsKey(user.Id))  // check for new user
+            {
+                _messageDataBase.Add(user.Id, new List<Message>());
+            }
+            _messageDataBase[user.Id].Add(message);
+        }
+
+        public List<Message> GetUserMessages(User user)
+        {
+            if(_messageDataBase.ContainsKey(user.Id)) // check user send any earlier messages
+            {
+                return _messageDataBase[user.Id];
+            }
+            return new List<Message>();
+        }
+
+        public bool UpdateUserMessages(User user, Message updatedMessage)
+        {
+            if(_messageDataBase.ContainsKey(user.Id))
+            {   
+                List<Message> data = _messageDataBase[user.Id];
+                for(int i = 0; i < data.Count; i++)
+                {   
+                    if(data[i].MessageId == updatedMessage.MessageId) // find the exist message
+                    {
+                        data[i] = updatedMessage;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+          
+        public Message? DeleteUserMessages(User user, Message message)
+        {
+            if(_messageDataBase.ContainsKey(user.Id))
+            {   
+                List<Message> data = _messageDataBase[user.Id];
+                for(int i = 0; i < data.Count; i++)
+                {   
+                    if(data[i].MessageId == message.MessageId)
+                    {
+                        Message deletedMsg = data[i];
+                        data.RemoveAt(i);   // remove that message user choosed
+                        return deletedMsg;
+                    }
+                }
+            }
+            throw new MessageNotFoundException($"Unable to delete message, Message not found : {message.MessageId}");
+
+        }
+    }
+}
