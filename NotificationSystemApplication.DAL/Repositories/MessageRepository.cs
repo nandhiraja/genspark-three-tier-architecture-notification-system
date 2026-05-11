d1b42c422fefa1a6c1db47f54893463af7c3e279
nandhiraja <nandhiraja16@gmail.com>
2026-05-11 22:07:50 +0530

    refactor: upgrade from In-Memory  to  DB data management

--- NotificationSystemApplication.DAL/Repositories/MessageRepository.cs
+++ NotificationSystemApplication.DAL/Repositories/MessageRepository.cs
@@ -1,66 +1,208 @@
+using System;
+using System.Collections.Generic;
 using NotificationSystemApplication.Core.CustomExceptions;
 using NotificationSystemApplication.Core.Interfaces;
 using NotificationSystemApplication.Core.Models;
+using Npgsql;
 
 namespace NotificationSystemApplication.DAL.Repositories
 {
-    // Repository for managing messages
+
+
     public class MessageRepository : IMessageRepository
     {
-        static Dictionary<string, List<Message>> _messageDataBase = new Dictionary<string, List<Message>>();
+        private readonly string _connectionString;
+
+        public MessageRepository()
+        {   
+            _connectionString = "Host=localhost;Port=5432;Database=NotificationAppDB;Username=nandhiraja;Password=";
+        }
+
+
+
+
+/// <summary>
+/// This function is used to add new message that user send
+/// </summary>
+/// <param name="user"></param>
+/// <param name="message"></param>
 
         public void AddUserMessage(User user, Message message)
-        {   
-            if(!_messageDataBase.ContainsKey(user.Id))  // check for new user
-            {
-                _messageDataBase.Add(user.Id, new List<Message>());
+        {
+            const string insertQuery = "INSERT INTO messages (sender_id, receiver_id, message_content, notification_mode, send_date) VALUES (@senderId, @receiverId, @content, @mode, @date);";
+            
+            try
+            {   
+                using var connection = new NpgsqlConnection(_connectionString);
+               
+                using var command = new NpgsqlCommand(insertQuery, connection);
+
+                command.Parameters.AddWithValue("@senderId", int.Parse(message.SenderId));
+                command.Parameters.AddWithValue("@receiverId", int.Parse(message.ReceiverId));
+                command.Parameters.AddWithValue("@content", message.MessageContent);
+                command.Parameters.AddWithValue("@mode", message.NotificationMode.ToString());
+                command.Parameters.AddWithValue("@date", message.Date.ToUniversalTime());
+
+                connection.Open();
+                int affectedRows = command.ExecuteNonQuery();
+                if (affectedRows > 0)
+                {
+                   Console.WriteLine("DB: Message sent successfully");
+                }
             }
-            _messageDataBase[user.Id].Add(message);
+
+            catch (Exception e)
+            {
+                throw new Exception($"Database Error: Failed to add message record. {e.Message}", e);
+            }
         }
 
+
+
+
+
+/// <summary>
+/// This function is used to get all user send messages
+/// </summary>
+/// <param name="user"></param>
+/// <returns> list of Mssages</returns>
         public List<Message> GetUserMessages(User user)
         {
-            if(_messageDataBase.ContainsKey(user.Id)) // check user send any earlier messages
-            {
-                return _messageDataBase[user.Id];
+            var userMessages = new List<Message>();
+            const string selectQuery = "SELECT message_id, sender_id, receiver_id, message_content, notification_mode, send_date FROM messages WHERE sender_id = @senderId ORDER BY send_date DESC;";
+
+            try
+            {   
+                using var connection = new NpgsqlConnection(_connectionString);
+                using var command = new NpgsqlCommand(selectQuery, connection);
+                
+                command.Parameters.AddWithValue("@senderId", int.Parse(user.Id));
+
+                connection.Open();
+                using var reader = command.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    var message = new Message
+                    {
+                        MessageId = reader.GetInt32(0).ToString(),
+                        SenderId = reader.GetInt32(1).ToString(),
+                        ReceiverId = reader.GetInt32(2).ToString(),
+                        MessageContent = reader.GetString(3),
+                        NotificationMode = Enum.Parse<NotificationType>(reader.GetString(4)),
+                        Date = reader.GetDateTime(5)
+                    };
+                    userMessages.Add(message);
+                }
+                return userMessages;
             }
-            return new List<Message>();
+
+
+            catch (Exception e)
+            {
+                throw new Exception($"Database Error: Failed to retrieve user chat records. {e.Message}", e);
+            }
         }
 
+
+
+
+
+
+/// <summary>
+/// This function are used to update the user send message
+/// </summary>
+/// <param name="user"></param>
+/// <param name="updatedMessage"></param>
+/// <returns>true/false based on action done</returns>
         public bool UpdateUserMessages(User user, Message updatedMessage)
         {
-            if(_messageDataBase.ContainsKey(user.Id))
-            {   
-                List<Message> data = _messageDataBase[user.Id];
-                for(int i = 0; i < data.Count; i++)
-                {   
-                    if(data[i].MessageId == updatedMessage.MessageId) // find the exist message
-                    {
-                        data[i] = updatedMessage;
-                        return true;
-                    }
-                }
+            const string updateQuery = "UPDATE messages SET message_content = @content, notification_mode = @mode WHERE message_id = @messageId AND sender_id = @senderId;";
+
+            try
+            {
+                using var connection = new NpgsqlConnection(_connectionString);
+                 
+                 using var command = new NpgsqlCommand(updateQuery, connection);
+
+                command.Parameters.AddWithValue("@content", updatedMessage.MessageContent);
+                command.Parameters.AddWithValue("@mode", updatedMessage.NotificationMode.ToString());
+                command.Parameters.AddWithValue("@messageId", int.Parse(updatedMessage.MessageId));
+                command.Parameters.AddWithValue("@senderId", int.Parse(user.Id));
+
+                connection.Open();
+                int affectedRows = command.ExecuteNonQuery();
+                 return affectedRows > 0;
             }
-            return false;
+            catch (Exception e)
+            {
+                throw new Exception($"Database Error: Failed to modify targeted message. {e.Message}", e);
+            }
         }
-          
+
+
+
+          /// <summary>
+          /// This function used to delete a message send by user
+          /// </summary>
+          /// <param name="user"></param>
+          /// <param name="message"></param>
+          /// <returns>deleted message</returns>
         public Message? DeleteUserMessages(User user, Message message)
         {
-            if(_messageDataBase.ContainsKey(user.Id))
-            {   
-                List<Message> data = _messageDataBase[user.Id];
-                for(int i = 0; i < data.Count; i++)
-                {   
-                    if(data[i].MessageId == message.MessageId)
+            const string selectQuery = "SELECT message_id, sender_id, receiver_id, message_content, notification_mode, send_date FROM messages WHERE message_id = @messageId AND sender_id = @senderId;";
+            const string deleteQuery = "DELETE FROM messages WHERE message_id = @messageId AND sender_id = @senderId;";
+
+            // validate ID before opening connection
+            if (!int.TryParse(message.MessageId, out int targetMessageId) || !int.TryParse(user.Id, out int senderId))
+            {
+                throw new ArgumentException("Invalid ID format");
+            }
+
+            try
+            {
+                Message? messageToDelete = null;
+
+                using var connection = new NpgsqlConnection(_connectionString);
+                connection.Open();
+
+                // Fetch record first
+                using (var selectCommand = new NpgsqlCommand(selectQuery, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@messageId", targetMessageId);
+                    selectCommand.Parameters.AddWithValue("@senderId", senderId);
+
+                    using var reader = selectCommand.ExecuteReader();
+                    if (reader.Read())
                     {
-                        Message deletedMsg = data[i];
-                        data.RemoveAt(i);   // remove that message user choosed
-                        return deletedMsg;
+                        messageToDelete = new Message
+                        {
+                            MessageId = reader.GetInt32(0).ToString(),
+                            SenderId = reader.GetInt32(1).ToString(),
+                            ReceiverId = reader.GetInt32(2).ToString(),
+                            MessageContent = reader.GetString(3),
+                            NotificationMode = Enum.Parse<NotificationType>(reader.GetString(4)),
+                            Date = reader.GetDateTime(5)
+                        };
                     }
+                } 
+                if (messageToDelete == null) return null;
+                // delete the message
+
+                using (var deleteCommand = new NpgsqlCommand(deleteQuery, connection))
+                {
+                    deleteCommand.Parameters.AddWithValue("@messageId", targetMessageId);
+                    deleteCommand.Parameters.AddWithValue("@senderId", senderId);
+
+                    deleteCommand.ExecuteNonQuery();
+                    
+                    return messageToDelete;
                 }
             }
-            throw new MessageNotFoundException($"Unable to delete message, Message not found : {message.MessageId}");
-
-        }
+            catch (Exception e)
+            {
+                throw new Exception($"DB Error: {e.Message}", e);
+            }
+        }       
     }
 }
--- NotificationSystemApplication.DAL/Repositories/UserRepository.cs
+++ NotificationSystemApplication.DAL/Repositories/UserRepository.cs
@@ -1,81 +1,197 @@
-using System.Security.Cryptography.X509Certificates;
-using System.Text.RegularExpressions;
-using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
 using NotificationSystemApplication.Core.CustomExceptions;
 using NotificationSystemApplication.Core.Interfaces;
 using NotificationSystemApplication.Core.Models;
+using Npgsql;
 
 namespace NotificationSystemApplication.DAL.Repositories
 {
-    public class UserRepository: IUserRepository
+    public class UserRepository : IUserRepository
     {
-        static List<User> _userDataBase = new List<User>();
+        private readonly string _connectionString;
 
-        public User AddUser(User user)
-        {
-           
-            _userDataBase.Add(user);
-            return user;
+        public UserRepository()
+        {   
+            // local db connection string 
+            _connectionString = "Host=localhost;Port=5432;Database=NotificationAppDB;Username=nandhiraja;Password=;";
         }
+
+        /// <summary>
+        /// This function used to find the new user 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>User</returns>
+        public User AddUser(User user)
+        {   
+            const string insertQuery = "INSERT INTO users (user_name, email, phone_number, create_at) VALUES (@name, @email, @phone, @date);";
+
+            try
+            {   // create a new connection
+                using var connection = new NpgsqlConnection(_connectionString);   // using var to automaticaly close/clear resource at end of this section
+                using var command = new NpgsqlCommand(insertQuery, connection);
+
+                command.Parameters.AddWithValue("@name", user.UserName);
+                command.Parameters.AddWithValue("@email", user.Email);
+                command.Parameters.AddWithValue("@phone", user.PhoneNumber);
+                command.Parameters.AddWithValue("@date", user.CreateAt.ToUniversalTime()); 
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"DB Error: Unable to insert user profile. {ex.Message}", ex);
+            }
+
+            return FindUserByEmail(user.Email);  // get the newly registered user with id
+        }
+        /// <summary>
+        /// This function used to find the exist user  in db by Email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>User</returns>
         public User FindUserByEmail(string email)
         {   
+            const string selectQuery = "SELECT id, user_name, email, phone_number, create_at FROM users WHERE email = @email;";
+
+            using var connection = new NpgsqlConnection(_connectionString);
+            using var command = new NpgsqlCommand(selectQuery, connection);
+            
+            command.Parameters.AddWithValue("@email", email);
+            connection.Open();
+            
+            using var reader = command.ExecuteReader();
+
+            if (!reader.HasRows)    // check any user has returned 
+            {
+               
+                throw new UserNotFoundException($"No user record matched the email address: {email}");  
+            }
+            //  execute when any user has fetched
+            var user = new User();
+            while (reader.Read()) 
+            {
+              
+                user.Id = reader.GetInt32(0).ToString(); 
+                user.UserName = reader.GetString(1);
+                user.Email = reader.GetString(2);
+                user.PhoneNumber = reader.IsDBNull(3) ? string.Empty : reader.GetString(3);
+                user.CreateAt = reader.GetDateTime(4);
            
-            foreach(var currentUser in _userDataBase)
+            } 
+            return user;
+        }
+        /// <summary>
+        /// This function used to find the exist user in db by id 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>User</returns>
+        public User FindUserById(string id)
+        {
+            const string selectQuery = "SELECT id, user_name, email, phone_number, create_at FROM users WHERE id = @id;";
+
+            if (!int.TryParse(id, out int numericId))
             {
-                if(currentUser.Email == email)   // find user with user email
-                {
-                  return currentUser;   
-                }
+                throw new ArgumentException("The provided ID is not a valid numeric identifier.", nameof(id));
             }
+
+            using var connection = new NpgsqlConnection(_connectionString);
+            using var command = new NpgsqlCommand(selectQuery, connection);
             
-            throw new UserNotFoundException($"User not found for email : {email}");
-        }
-         public User FindUserById(string id)
-        {
-            foreach(var currentUser in _userDataBase)
+            command.Parameters.AddWithValue("@id", numericId);
+           
+           
+            connection.Open();
+            
+            using var reader = command.ExecuteReader();
+
+            if (!reader.HasRows)
             {
-                if(currentUser.Id == id)   // find user with user id
-                {
-                  return currentUser;   
-                }
+                throw new UserNotFoundException($"No user record matched the user ID: {id}");
             }
-            throw new UserNotFoundException($"User not found for Id : {id}");
-        }
 
-        public bool UpdateUser(string email, User newUpdateUser)
-        {
-            int length =_userDataBase.Count;
-
-            for (int i =0;i<length;i++) // get the index of user in db to update
+            var user = new User();
+            while (reader.Read())
             {
-                if(_userDataBase[i].Email==email)
-                {
-                    _userDataBase[i] = newUpdateUser;
-                    return true;
-                }
+
+                user.Id = reader.GetInt32(0).ToString();
+                user.UserName = reader.GetString(1);
                 
-            }
-            return false;
-            
+                user.Email = reader.GetString(2);
+                user.PhoneNumber = reader.IsDBNull(3) ? string.Empty : reader.GetString(3);
+                user.CreateAt = reader.GetDateTime(4);
+            } 
+            return user;
         }
 
+/// <summary>
+/// This function used to update the existing user 
+/// </summary>
+/// <param name="email"></param>
+/// <param name="updatedUser"></param>
+/// <returns>bool</returns>
+        
+        public bool UpdateUser(string email, User updatedUser)
+        {   
+            
+            const string updateQuery = "UPDATE users SET user_name = @name, email = @newEmail, phone_number = @phone WHERE email = @oldEmail;";
 
+            try
+            {   
+                using var connection = new NpgsqlConnection(_connectionString);
+               
+                using var command = new NpgsqlCommand(updateQuery, connection);
+
+                command.Parameters.AddWithValue("@name", updatedUser.UserName);
+                command.Parameters.AddWithValue("@newEmail", updatedUser.Email); 
+                command.Parameters.AddWithValue("@phone", updatedUser.PhoneNumber);
+                command.Parameters.AddWithValue("@oldEmail", email);
+
+                connection.Open();
+                int affectedRows = command.ExecuteNonQuery();
+                 return affectedRows > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"DB Error: could not execute update logic {ex.Message}", ex);
+            }
+        }
+/// <summary>
+/// This function is used to delete the existing user 
+/// </summary>
+/// <param name="email"></param>
+/// <returns>user</returns>
         public User? DeleteUser(string email)
         {
-            int length =_userDataBase.Count;
-
-            for (int i =0;i<length;i++)
+            User user;
+            try 
             {
-                if(_userDataBase[i].Email==email)
-                {   User currentUser = _userDataBase[i];
-                    _userDataBase.RemoveAt(i);
-                    return currentUser;
-                }
-                
+                user = FindUserByEmail(email);    // check the user if register or not
             }
-            throw new UserNotFoundException($"User not found for email : {email}, Unable to remove user");
+            catch (UserNotFoundException)
+             {
+                return null; 
+            }
 
-            
+            const string deleteQuery = "DELETE FROM users WHERE email = @email;";
+
+            try
+            {   
+                using var connection = new NpgsqlConnection(_connectionString);
+               
+                using var command = new NpgsqlCommand(deleteQuery, connection);
+
+                command.Parameters.AddWithValue("@email", email);
+                connection.Open();
+                
+                int affectedRows = command.ExecuteNonQuery();
+                 return affectedRows > 0 ? user : null;    // check if user has deleted or not
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"DB Error: Unable to delete for email {email}. {ex.Message}", ex);
+            }
         }
     }
 }
